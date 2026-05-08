7ccc1c54c394170f4877a27b91a6cfe05de3888c
Asís <38328023+soyasis@users.noreply.github.com>
2026-05-08 19:29:55 +0200

    cai 1.5.0

--- Casks/cai.rb
+++ Casks/cai.rb
@@ -1,6 +1,6 @@
 cask "cai" do
-  version "1.4.0"
-  sha256 "3bf47b2ed501eccb18a2df75fc643221c80efdbc7fd13941b3b2a6c82fa81bef"
+  version "1.5.0"
+  sha256 "e01c287d9c2969965ab32b5d267847296803ef1f532a085655e6655bcc1614c0"
 
   url "https://github.com/cai-layer/cai/releases/download/v#{version}/Cai-#{version}-macos.dmg"
   name "Cai"
